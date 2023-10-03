20f2d1aaab37167bd73490ae816f040b1c3ed383
yaroslawv-nexus <124203359+yaroslawv-nexus@users.noreply.github.com>
2023-10-03 22:38:37 +0300

    fix

--- src/redux/asyncRedax.js
+++ src/redux/asyncRedax.js
@@ -18,8 +18,7 @@ export const fetchContacts = createAsyncThunk("tasks/fetchAll", async (_, thunkA
 export const addContact = createAsyncThunk("contacts/addContact", async (contact, thunkAPI) => {
   try {
     const response = await axios.post("/contacts", contact);
-  
-  return response.data;
+    return response.data;
   }
   catch (e) {
     return thunkAPI.rejectWithValue(e.message);
@@ -29,8 +28,8 @@ export const addContact = createAsyncThunk("contacts/addContact", async (contact
 
 export const deleteContact = createAsyncThunk("contacts/deleteContact", async (id, thunkAPI) => {
   try {
-  const response = await axios.delete(`/contacts/${id}`);
-  return response.data;
+    const response = await axios.delete(`/contacts/${id}`);
+    return response.data;
   }
   catch (e) {
     return thunkAPI.rejectWithValue(e.message);
